fe1e354eaa3159e12be484335c67953479a3a007
Fritz <fritz.developer10@gmail.com>
2023-07-09 21:45:12 -0500

    description del bingo

---
+++ main.js
@@ -0,0 +1,8 @@
+// El programa trata de un bingo en donde se tiene que escoger un numero de 1 al 10
+// si es el mismo numero debe devolver bingo!!!, sino pues le debe dar tres intentos mas
+// despues de los tres intentod pues debe decir lo siento vuelve a jugar otra ronda.
+
+
+const getAleatorio2 = () => Math.random();
+
+console.log(getAleatorio2());
