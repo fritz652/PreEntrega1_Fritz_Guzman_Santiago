2a4f95417183bd62a8aac66520c01390a50f6cd6
Fritz <fritz.developer10@gmail.com>
2023-07-10 23:37:08 -0500

    juego adivinar numero

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-// El programa trata de un bingo en donde se tiene que escoger un numero de 1 al 10
-// si es el mismo numero debe devolver bingo!!!, sino pues le debe dar tres intentos mas
-// despues de los tres intentod pues debe decir lo siento vuelve a jugar otra ronda.
+function juegoAdivinarNumero() {
+    const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    let intentos = 0;
+    let adivinado = false;
 
+    while (!adivinado && intentos < 5) {
+        const numeroElegido = parseInt(prompt("Intento #" + (intentos + 1) + ": Adivina el número (del 1 al 100):"));
 
-const getAleatorio2 = () => Math.random();
+        if (isNaN(numeroElegido) || numeroElegido < 1 || numeroElegido > 100) {
+            alert("Por favor, ingresa un número válido del 1 al 100.");
+            continue;
+        }
 
-console.log(getAleatorio2());
+        if (numeroElegido === numeroSecreto) {
+            alert("¡Felicitaciones! ¡Adivinaste el número secreto!");
+            adivinado = true;
+        } else if (numeroElegido < numeroSecreto) {
+            alert("El número secreto es mayor. Sigue intentando.");
+        } else {
+            alert("El número secreto es menor. Sigue intentando.");
+        }
+
+        intentos++;
+    }
+
+    if (!adivinado) {
+        alert("Lo siento, has agotado tus intentos. El número secreto era: " + numeroSecreto);
+    }
+}
+
+juegoAdivinarNumero();
